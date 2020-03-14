67ce6d88de75b6ab1262fcf030c0a2f42ef24c31
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2020-03-14 11:49:06 +0900

    Step 7. Write messages to Cloud Firestore - Listen to the form submission

--- firebase-get-to-know-web/index.js
+++ firebase-get-to-know-web/index.js
@@ -77,3 +77,20 @@ firebase.auth().onAuthStateChanged((user)=> {
     startRsvpButton.textContent = "RSVP"
   }
 });
+
+// Listen to the form submission
+form.addEventListener("submit", (e) => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  firebase.firestore().collection("guestbook").add({
+    text: input.value,
+    timestamp: Date.now(),
+    name: firebase.auth().currentUser.displayName,
+    userId: firebase.auth().currentUser.uid
+  })
+  // clear message input field
+  input.value = ""; 
+  // Return false to avoid redirect
+  return false;
+});
