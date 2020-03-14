9af2c66152707afce08d7c64a11af09520512d3e
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2020-03-14 13:24:31 +0900

    9. Set up basic security rules - Reset listeners

--- firebase-get-to-know-web/index.js
+++ firebase-get-to-know-web/index.js
@@ -74,11 +74,15 @@ firebase.auth().onAuthStateChanged((user)=> {
     startRsvpButton.textContent = "LOGOUT"
     // Show guestbook to logged-in users
     guestbookContainer.style.display = "block";
+    // Subscribe to the guestbook collection
+    subscribeGuestbook();
   }
   else {
     startRsvpButton.textContent = "RSVP"
     // Hide guestbook for non-logged-in users
     guestbookContainer.style.display = "none";
+    // Unsubscribe from the guestbook collection
+    unsubscribeGuestbook();
   }
 });
 
@@ -99,17 +103,29 @@ form.addEventListener("submit", (e) => {
   return false;
 });
 
-// Create query for messages
-firebase.firestore().collection("guestbook")
-  .orderBy("timestamp","desc")
-  .onSnapshot((snaps) => {
-    // Reset page
-    guestbook.innerHTML = "";
-    // Loop through documents in database
-    snaps.forEach((doc) => {
-      // Create an HTML entry for each document and add it to the chat
-      const entry = document.createElement("p");
-      entry.textContent = doc.data().name + ": " + doc.data().text;
-      guestbook.appendChild(entry);
-    });
-  });
+// Listen to guestbook updates
+function subscribeGuestbook(){
+   // Create query for messages
+ guestbookListener = firebase.firestore().collection("guestbook")
+ .orderBy("timestamp","desc")
+ .onSnapshot((snaps) => {
+   // Reset page
+   guestbook.innerHTML = "";
+   // Loop through documents in database
+   snaps.forEach((doc) => {
+     // Create an HTML entry for each document and add it to the chat
+     const entry = document.createElement("p");
+     entry.textContent = doc.data().name + ": " + doc.data().text;
+     guestbook.appendChild(entry);
+   });
+ });
+};
+
+// Unsubscribe from guestbook updates
+function unsubscribeGuestbook(){
+ if (guestbookListener != null)
+ {
+   guestbookListener();
+   guestbookListener = null;
+ }
+};
